81c725c8ab972b6e9f94c1e97c0332bfde3ca7ae
jabobinski <bobinski00@gmail.com>
2024-04-23 11:47:20 +0200

    Zadanie

---
+++ z1.py
@@ -0,0 +1,5 @@
+# Prompting user for input and calculating the length directly
+string_length = len(input("Enter a string: "))
+
+# Printing the length of the string
+print("Length of the string:", string_length)
